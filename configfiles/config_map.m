d6f3e93459a060eb11e5c9de07fc5f901dfb2c8d
Daniel Emanuelsson <d.emanuelsson@gmail.com>
2022-09-27 18:50:52 +0200

    Update config_map.m

--- configfiles/config_map.m
+++ configfiles/config_map.m
@@ -1,4 +1,10 @@
 % config_map
 
+filename='Figure_1_map_z500_SAT_SIC_EOF'; 
+filedir ='C:\Users\Machine\matlab_storage_of_output_files\figures\';
+
 letter='';
 letter_position=  [120.1 832 48 46];
+
+
+    qual_str='-r250';
