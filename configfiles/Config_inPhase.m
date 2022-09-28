cc11c5f52993841214a9bee420a11a8f610b6c3a
Daniel Emanuelsson <d.emanuelsson@gmail.com>
2022-09-28 19:08:15 +0200

    Create Config_inPhase.m

---
+++ configfiles/Config_inPhase.m
@@ -0,0 +1,14 @@
+% Config_inPhase
+
+data_dir='C:\Users\Machine\matlab_lib\Data\ERA_interim\';
+filedir ='C:\Users\Machine\matlab_storage_of_output_files\';
+
+
+yr_s=1979;
+yr_e=2011;
+letter='a';
+letter_position=   [240 380 60 60];
+
+
+
+
