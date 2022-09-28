94a4ed2933cc6bd66800d1012d34cd7c2f025e5e
Daniel Emanuelsson <d.emanuelsson@gmail.com>
2022-09-28 17:54:15 +0200

    Create Config_corr_Z500.m

---
+++ configfiles/Config_corr_Z500.m
@@ -0,0 +1,10 @@
+% Config_corr_Z500
+
+data_dir='C:\Users\Machine\matlab_lib\Data\ERA_interim\';
+filedir ='C:\Users\Machine\matlab_storage_of_output_files\';
+
+
+era_name_nr=1;
+sea_nr=3; 
+iso_alt_nr=3;
+
