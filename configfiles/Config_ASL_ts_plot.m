abe70793e73697074aab00add8577cd9164b2af0
Daniel Emanuelsson <d.emanuelsson@gmail.com>
2022-09-28 17:54:13 +0200

    Create Config_ASL_ts_plot.m

---
+++ configfiles/Config_ASL_ts_plot.m
@@ -0,0 +1,15 @@
+% Config_ASL_ts_plot
+
+data_dir='C:\Users\Machine\matlab_lib\Data\ERA_interim\';
+filedir ='C:\Users\Machine\matlab_storage_of_output_files\';
+
+name='z500';
+dataset=1; % (1) ERA-interim (2) NCEP-NCAR  %%%%%%%%%%%%%
+sea_nr=6;
+max_ASL_point=1; % (0/1)
+max_nr=1;
+
+iso_alt_nr=2;
+
+ letter='b';
+ letter_position=   [200 380 50 60];
