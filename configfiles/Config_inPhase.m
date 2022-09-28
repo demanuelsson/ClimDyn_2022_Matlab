4e29cee322d20793f8796c12aff0a7f0c7184348
Daniel Emanuelsson <d.emanuelsson@gmail.com>
2022-09-28 19:56:17 +0200

    Update Config_inPhase.m

--- configfiles/Config_inPhase.m
+++ configfiles/Config_inPhase.m
@@ -4,10 +4,15 @@ data_dir='C:\Users\Machine\matlab_lib\Data\ERA_interim\';
 filedir ='C:\Users\Machine\matlab_storage_of_output_files\';
 
 
+era_name_nr=2; % 1 Z500 or 2 SAT
+
 yr_s=1979;
 yr_e=2011;
 letter='a';
 letter_position=   [240 380 60 60];
+
+
+sea_nr=3;    
 
 
 
