1b5ad231fe40fb083db3d70d77a3e4e53f2b2bdd
Daniel Emanuelsson <d.emanuelsson@gmail.com>
2022-09-28 19:08:11 +0200

    Create Config_corr_SIC.m

---
+++ configfiles/Config_corr_SIC.m
@@ -0,0 +1,13 @@
+% Config_corr_SIC
+
+data_dir='C:\Users\Machine\matlab_lib\Data\HadISST_data\';
+filedir ='C:\Users\Machine\matlab_storage_of_output_files\';
+
+
+ site='RICE';
+  yr_s=1979;
+  yr_e=2011; 
+
+sea_nr=3; 
+iso_alt_nr=3;
+
