acebb049b2cc784bb3e92f33344e334da5cf6df2
Daniel Emanuelsson <d.emanuelsson@gmail.com>
2022-09-29 16:36:31 +0200

    Create Config_corr_SAT_Suppl_M.m

---
+++ configfiles/Config_corr_SAT_Suppl_M.m
@@ -0,0 +1,14 @@
+% Config_corr_SAT
+
+data_dir='C:\Users\Machine\matlab_lib\Data\ERA_interim\';
+filedir ='C:\Users\Machine\matlab_storage_of_output_files\';
+
+
+era_name_nr=2;
+sea_nr=3; 
+iso_alt_nr=3;
+
+yr_s=1979;
+yr_e=2009;
+
+
