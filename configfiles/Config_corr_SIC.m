81697075f424b1a3865519ee792c53544ba34ad0
Daniel Emanuelsson <d.emanuelsson@gmail.com>
2022-10-01 18:14:58 +0200

    Update Config_corr_SIC.m

--- configfiles/Config_corr_SIC.m
+++ configfiles/Config_corr_SIC.m
@@ -3,11 +3,18 @@
 data_dir='C:\Users\Machine\matlab_lib\Data\HadISST_data\';
 filedir ='C:\Users\Machine\matlab_storage_of_output_files\';
 
-
+ name='SIC';
  site='RICE';
   yr_s=1979;
   yr_e=2011; 
 
-sea_nr=3; 
+sea_nr=3; % AMJJASON
 iso_alt_nr=3;
+
+  proj='stereo';
+  lat1=-90;
+  lat2=-50;
+  box_use=0;  % (0/1)  
+
+letter='e';
 
