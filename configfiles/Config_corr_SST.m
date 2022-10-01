4895362199bbef4320d8487420a589eca8c2e836
Daniel Emanuelsson <d.emanuelsson@gmail.com>
2022-10-01 18:33:34 +0200

    Create Config_corr_SST.m

---
+++ configfiles/Config_corr_SST.m
@@ -0,0 +1,25 @@
+% Config_corr_SIC
+
+data_dir='C:\Users\Machine\matlab_lib\Data\HadISST_data\';
+filedir ='C:\Users\Machine\matlab_storage_of_output_files\';
+
+ name='SST';
+ site='RICE';
+  yr_s=1979;
+  yr_e=2011; 
+
+sea_nr=3; % AMJJASON
+iso_alt_nr=3;
+
+  proj='mercator';
+  lat1=-90;
+  lat2=-50;
+  lon1=0;
+  lon2= 360;
+  box_use=0;  % (0/1)
+  RSAS_SIC_box=0;
+  
+  color_alt=6; 
+
+letter='e';
+
