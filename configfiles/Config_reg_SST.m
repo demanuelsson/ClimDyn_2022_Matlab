7a8d3e3dd2670832926f1d3f7e665d5dd63f1f5e
Daniel Emanuelsson <d.emanuelsson@gmail.com>
2022-10-02 07:37:17 +0200

    Create Config_reg_SST.m

---
+++ configfiles/Config_reg_SST.m
@@ -0,0 +1,28 @@
+% Config_corr_SIC
+
+data_dir='C:\Users\Machine\matlab_lib\Data\HadISST_data\';
+filedir ='C:\Users\Machine\matlab_storage_of_output_files\';
+
+ name='SST';
+ site='RICE';
+  yr_s=1979;
+  yr_e=2011; 
+
+sea_nr=7; % AMJJASON
+ season='AMJJASON';
+ PC_nr=3; 
+
+iso_alt_nr=3;
+
+  proj='mercator';
+  lat1=-90;
+  lat2=0;
+  lon1=0;
+  lon2= 360;
+  box_use=0;  % (0/1)
+  RSAS_SIC_box=0;
+  
+  color_alt=4; 
+
+letter='e';
+
