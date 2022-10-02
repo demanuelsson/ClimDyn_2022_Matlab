62f450f50edfe232fe99da4aa5244436dc894a2b
Daniel Emanuelsson <d.emanuelsson@gmail.com>
2022-10-02 08:14:07 +0200

    Update Config_reg_SST.m

--- configfiles/Config_reg_SST.m
+++ configfiles/Config_reg_SST.m
@@ -6,7 +6,7 @@ filedir ='C:\Users\Machine\matlab_storage_of_output_files\';
  name='SST';
  site='RICE';
   yr_s=1979;
-  yr_e=2011; 
+  yr_e=2012; 
 
 sea_nr=7; % AMJJASON
  season='AMJJASON';
@@ -14,15 +14,22 @@ sea_nr=7; % AMJJASON
 
 iso_alt_nr=3;
 
+  proj_nr=2;
   proj='mercator';
   lat1=-90;
-  lat2=0;
+  lat2=10;
   lon1=0;
   lon2= 360;
   box_use=0;  % (0/1)
   RSAS_SIC_box=0;
   
-  color_alt=4; 
+  color_alt=7; 
+  c_limit=0.3;
+  
+  co_pa=[.1 .1 .1];  % contour   
+  line_w_nr=2;
 
 letter='e';
+
+coast_nr=1;
 
