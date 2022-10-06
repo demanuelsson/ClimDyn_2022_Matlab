f47064b183f158f995326f4c0ef112d6baf1a07d
Daniel Emanuelsson <d.emanuelsson@gmail.com>
2022-10-06 07:38:05 +0200

    Update Config_reg_SST.m

--- configfiles/Config_reg_SST.m
+++ configfiles/Config_reg_SST.m
@@ -11,9 +11,9 @@ filedir ='C:\Users\Machine\matlab_storage_of_output_files\';
   yr_s=1979;
   yr_e=2011; 
 
-sea_nr=7; % AMJJASON
+ sea_nr=7; % AMJJASON
  season='AMJJASON';
- PC_nr=2; % (3) SAM (3) PSA1 (4) PSA2
+ PC_nr=3; % (2) SAM (3) PSA1 [use] (4) PSA2
 
  set_yr=1;
  
@@ -28,7 +28,7 @@ sea_nr=7; % AMJJASON
   lat1=-90;
   lat2=20;
   lon1=0;
-  lon2= 360;
+  lon2= 359.9;
   box_use=0;  % (0/1)
   RSAS_SIC_box=0;
   
@@ -47,9 +47,9 @@ sea_nr=7; % AMJJASON
   co_pa=[.88 .18 .88];  % contour   
   line_w_nr=2;
 
-letter='';
+letter='c';
 letter_pos= [175 525 50 50];
-letter_size=28;
+letter_size=22;%28
 
 coast_nr=1;
 
@@ -57,3 +57,9 @@ x_lab=0.96;y_lab=0.63; % unit position
 
 show_title=0;
 Nino_34=1; % show nino-34 box
+
+label_size=20;
+label_vert=-300;
+label_across=-64.9;
+%label_across=19.99;
+
