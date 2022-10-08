72b552d9ca9cec3ea8b003fbcba97599b078605c
Daniel Emanuelsson <d.emanuelsson@gmail.com>
2022-10-08 11:09:56 +0200

    Update Config_reg_EOF.m

--- configfiles/Config_reg_EOF.m
+++ configfiles/Config_reg_EOF.m
@@ -2,3 +2,11 @@
 
 data_dir='C:\Users\Machine\matlab_lib\Data\ERA_interim\';
 filedir ='C:\Users\Machine\matlab_storage_of_output_files\';
+
+
+era_name_nr=1;
+sea_nr=7; %%%%%% (1) Annual (4) JJA (5) SON (6) MAMJJASON (7) AMJJASON
+
+
+proj='stereo';
+color_alt=6;
